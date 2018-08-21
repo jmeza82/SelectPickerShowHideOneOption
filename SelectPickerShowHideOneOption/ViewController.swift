8badb450999b791d9de61a5a2b9dd123c935c1cf
Juan Meza <caborcarescue@yahoo.com.mx>
2018-08-20 23:13:54 -0700

    Select PickerView Hide

--- SelectPickerShowHideOneOption/ViewController.swift
+++ SelectPickerShowHideOneOption/ViewController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerTextField: UITextField!
+    
+    let pickOptions = ["One", "Two", "Three", "Seven", "Fifteen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let pickerView = UIPickerView()
+        
+        pickerView.delegate = self
+        
+        pickerTextField.inputView = pickerView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return pickOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return pickOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        pickerTextField.text = pickOptions[row]
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        view.endEditing(true)
     }
 
 
